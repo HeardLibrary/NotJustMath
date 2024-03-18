e96cf808edf76cee77139342f6c0552e146b9237
Alex Barrera-Waters <alexbawa@Alexs-MacBook-Air-10.local>
2024-03-18 14:12:53 -0500

    Get basic PDF viewer working

--- src/pages/LessonPage/LessonPage.js
+++ src/pages/LessonPage/LessonPage.js
@@ -4,12 +4,24 @@ import { useParams } from "react-router-dom";
 import { getUrl } from "aws-amplify/storage";
 import Header from "../../components/Header/Header";
 import "./LessonPage.css";
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+const pdfjs = await import('pdfjs-dist/build/pdf');
+const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.min');
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
 
 const LessonPage = () => {
     const { lessonID } = useParams();
     const [pdfLink, setPDFLink] = useState(null);
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
 
     useEffect(() => {
         async function getAndSetPDFLink () {
@@ -24,15 +36,31 @@ const LessonPage = () => {
         getAndSetPDFLink();
     }, [lessonID])
 
+    const nextPage = () => {
+        if (pageNumber < numPages) {
+            setPageNumber(pageNumber+1)
+        }
+    }
+
+    const previousPage = () => {
+        if (pageNumber > 0) {
+            setPageNumber(pageNumber-1)
+        }
+    }
+
     function renderPDFViewer() {
         if(pdfLink) {
             console.log("PDF link generated: ", pdfLink)
             return (
                 <div>
-                    <Document file={pdfLink}>
+                    <Document file={pdfLink} onLoadSuccess={onDocumentLoadSuccess}>
                         <Page pageNumber={pageNumber}/>
                     </Document>
                     <p>{pageNumber} of {numPages}</p>
+                    <div className="navigation-container">
+                        <button onClick={previousPage}>Previous</button>
+                        <button onClick={nextPage}>Next</button>
+                    </div>
                 </div>
             )
         } else {
