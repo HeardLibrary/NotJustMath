64c97e1350ad21d7ebe9aa32d1dfe76bb8713709
Alex Barrera-Waters <alexbawa@Alexs-MacBook-Air-10.local>
2024-03-20 10:50:24 -0500

    Get working styles for result preview

--- src/components/LessonPlanResultPreview/LessonPlanResultPreview.js
+++ src/components/LessonPlanResultPreview/LessonPlanResultPreview.js
@@ -5,7 +5,7 @@ const LessonPlanResultPreview = (props) => {
     return (
         <Link className="result-preview" to={`/lesson/${props.lessonPlanMetadata.id}`}>
             <div className="result-preview-left">
-                <p className="result-preview-title">{props.lessonPlanMetadata.lesson_title}</p>
+                <p className="result-preview-title">{props.lessonPlanMetadata.lesson_title} (Grades {props.lessonPlanMetadata.grade_level_lower} - {props.lessonPlanMetadata.grade_level_upper})</p>
                 <p className="result-preview-tags"><span className="tag-title">Math Concepts:</span> {props.lessonPlanMetadata.math_concept_tags.join(", ")}</p>
                 <p className="result-preview-tags"><span className="tag-title">Social Concepts:</span> {props.lessonPlanMetadata.social_concept_tags.join(", ")}</p>
                 <p className="result-preview-tags"><span className="tag-title">Math Content Standards:</span> {props.lessonPlanMetadata.standard_tags.join(", ")}</p>
