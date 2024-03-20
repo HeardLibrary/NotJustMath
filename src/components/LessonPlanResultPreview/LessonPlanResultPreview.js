bf49ec06f8590e6d1cbdca36a5ba04538d851dcd
Alex Barrera-Waters <alexbawa@alexs-air-10.lan>
2024-03-19 22:14:55 -0500

    Start styling search results

--- src/components/LessonPlanResultPreview/LessonPlanResultPreview.js
+++ src/components/LessonPlanResultPreview/LessonPlanResultPreview.js
@@ -4,7 +4,13 @@ import "./LessonPlanResultPreview.css"
 const LessonPlanResultPreview = (props) => {
     return (
         <Link className="result-preview" to={`/lesson/${props.lessonPlanMetadata.id}`}>
-            <p>Title: {props.lessonPlanMetadata.lesson_title}</p>
+            <div className="result-preview-left">
+                <p className="result-preview-title">{props.lessonPlanMetadata.lesson_title}</p>
+                <p className="result-preview-tags"><span className="tag-title">Math Concepts:</span> {props.lessonPlanMetadata.math_concept_tags.join(", ")}</p>
+                <p className="result-preview-tags"><span className="tag-title">Social Concepts:</span> {props.lessonPlanMetadata.social_concept_tags.join(", ")}</p>
+                <p className="result-preview-tags"><span className="tag-title">Math Content Standards:</span> {props.lessonPlanMetadata.standard_tags.join(", ")}</p>
+            </div>
+            <div className="result-preview-right"></div>
         </Link>
     )
 }
