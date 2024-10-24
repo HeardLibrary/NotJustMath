ecd386e2bbfe8ac8db7f5d5362b343bf08634a06
huongthaoh <121412317+huongthaoh@users.noreply.github.com>
2024-10-24 17:30:48 -0500

    test

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import App from "./App";
 import config from "./amplifyconfiguration.json";
 import { Amplify } from "aws-amplify";
 
-//test pushing this to github
-
 Amplify.configure(config);
+
+//testing branch protection 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
