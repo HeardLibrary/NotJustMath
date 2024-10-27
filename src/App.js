663ddf0c1a7e36cefd12ad81bc3c9c1821a7219f
huongthaoh <121412317+huongthaoh@users.noreply.github.com>
2024-10-27 18:09:28 -0500

    refactor routing logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import GuidePage from './pages/GuidePage/GuidePage';
 import HomePage from './pages/HomePage/HomePage';
 import LessonPage from './pages/LessonPage/LessonPage';
@@ -7,6 +7,7 @@ import UploadPage from "./pages/UploadPage/UploadPage";
 import AdminPage from "./pages/AdminPage/AdminPage";
 import BadRequestPage from "./pages/BadRequestPage/BadRequestPage";
 import './App.css';
+import Header from "./components/Header/HeaderNew";
 
 const pdfjs = await import('pdfjs-dist/build/pdf');
 const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.min');
@@ -14,42 +15,23 @@ const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.min');
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navigate to="/home" replace/>
-    },
-    {
-      path: "/home",
-      element: <HomePage/>
-    },
-    {
-      path: "/guide",
-      element: <GuidePage/>
-    },
-    {
-      path: "/search",
-      element: <SearchPage/>
-    },
-    {
-      path: "/upload",
-      element: <UploadPage/>
-    },
-    {
-      path: "/lesson/:lessonID",
-      element: <LessonPage/>
-    },
-    {
-      path: "/admin",
-      element: <AdminPage/>
-    },
-    {
-      path: "*",
-      element: <BadRequestPage/>
-    },
-  ]);
-
-  return <RouterProvider className="app-wrapper" router={router}/>
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/guide" element={<GuidePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/lesson/:lessonID" element={<LessonPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="*" element={<BadRequestPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
