664dcc55643ab24ff737564eccc20686d3de28c0
huongthaoh <121412317+huongthaoh@users.noreply.github.com>
2024-10-28 01:07:11 -0500

    add image carousel

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -5,18 +5,6 @@ const ImageCarousel = ({ images }) => {
     return (
         <div className="carousel-container">
             <Carousel arrows infinite={false}>
-                {/* <div>
-                    <img src={ } className="carousel-image">1</img>
-                </div>
-                <div>
-                    <img src={ } className="carousel-image">2</img>
-                </div>
-                <div>
-                    <img src={ } className="carousel-image">3</img>
-                </div>
-                <div>
-                    <img src={ } className="carousel-image">4</img>
-                </div> */}
                 {images.map((src, index) => (
                     <div key={index}>
                         <img src={src} alt={`Slide ${index + 1}`} className="carousel-image" />
