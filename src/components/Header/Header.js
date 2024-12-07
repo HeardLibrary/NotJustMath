9bb9784dcde9d98562c52dd7ab53ea9fd3c78312
gutoleeofficial <augusto.leee@gmail.com>
2024-12-07 13:18:15 -0600

    fix: deleted all things related to the guide page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,36 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import Logo from "./assets/logo.png";
-import "./Header.css"
+import "./Header.css";
 
-const ALL_PAGES = [
-    "home",
-    "guide",
-    "search",
-    "upload",
-    "admin"
-]
+const ALL_PAGES = ["home", "search", "upload", "admin"];
 
 const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="header-container">
-            <div className="header-title">
-                <img src={Logo} alt="Page logo"/>
-                <p>[Not] Just Math</p>
-            </div>
-            
-            <div className="header-options">
-                {ALL_PAGES.map(page => {
-                    return <button className="header-option"key={page} onClick={() => {navigate(`/${page}`)}}>{page.charAt(0).toUpperCase() + page.slice(1)}</button>
-                })}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header-container">
+      <div className="header-title">
+        <img src={Logo} alt="Page logo" />
+        <p>[Not] Just Math</p>
+      </div>
+
+      <div className="header-options">
+        {ALL_PAGES.map((page) => {
+          return (
+            <button
+              className="header-option"
+              key={page}
+              onClick={() => {
+                navigate(`/${page}`);
+              }}
+            >
+              {page.charAt(0).toUpperCase() + page.slice(1)}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
